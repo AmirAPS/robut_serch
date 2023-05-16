b59c1128a825cde5c77458fa229fc2e7c0e64b1a
AmirAPS <133797001+AmirAPS@users.noreply.github.com>
2023-05-16 20:13:30 +0330

    Add files via upload
    این یک ربات تحقیقگر است

---
+++ Robot researcher.py
@@ -0,0 +1,13 @@
+
+import wikipedia
+import pyttsx3 as pt
+while True:
+    pt.speak('Hello, welcome to APS Researcher Robot')
+    while True:
+        qw = input('waths the serch?')
+        qa = wikipedia.summary(qw)
+        print(qa)
+
+        sa = input('Do you want to exit? yes or no?')
+        if (sa == 'yes'):
+         break
