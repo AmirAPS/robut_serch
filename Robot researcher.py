1fc23e0ee80c8efb8ac0abbe8e8a8e114ae28ff9
AmirParsa <133797001+AmirAPS@users.noreply.github.com>
2023-05-16 21:53:22 +0330

    Update Robot researcher.py

--- Robot researcher.py
+++ Robot researcher.py
@@ -1,13 +1,12 @@
 
 import wikipedia
 import pyttsx3 as pt
+pt.speak('Hello, welcome to APS Researcher Robot')
 while True:
-    pt.speak('Hello, welcome to APS Researcher Robot')
-    while True:
-        qw = input('waths the serch?')
-        qa = wikipedia.summary(qw)
-        print(qa)
+       qw = input('waths the serch?')
+       qa = wikipedia.summary(qw)
+       print(qa)
 
-        sa = input('Do you want to exit? yes or no?')
-        if (sa == 'yes'):
-         break
+       sa = input('Do you want to exit? yes or no?')
+       if (sa == 'yes'):
+        break
